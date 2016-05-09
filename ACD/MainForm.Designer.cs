720d031652c03b076866db74706e74c8f889bdcf
manish mallavarapu <manish101096@gmail.com>
2016-05-09 12:11:59 -0700

    Added updated gui

--- ACD/MainForm.Designer.cs
+++ ACD/MainForm.Designer.cs
@@ -92,23 +92,23 @@
             this.panel1.Controls.Add(this.comboBox2);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Location = new System.Drawing.Point(3, 110);
+            this.panel1.Location = new System.Drawing.Point(3, 127);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(232, 131);
             this.panel1.TabIndex = 4;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(147, 90);
+            this.button3.Location = new System.Drawing.Point(88, 103);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 14;
-            this.button3.Text = "delete";
+            this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(152, 3);
+            this.button4.Location = new System.Drawing.Point(6, 103);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 13;
@@ -119,7 +119,7 @@
             // comboBox3
             // 
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(20, 92);
+            this.comboBox3.Location = new System.Drawing.Point(6, 78);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(121, 21);
             this.comboBox3.TabIndex = 7;
@@ -127,7 +127,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(17, 76);
+            this.label4.Location = new System.Drawing.Point(3, 62);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 6;
@@ -140,7 +140,7 @@
             "Discipline Content",
             "Cognitive Skill",
             "Habit of Mind"});
-            this.comboBox2.Location = new System.Drawing.Point(20, 52);
+            this.comboBox2.Location = new System.Drawing.Point(6, 38);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(121, 21);
             this.comboBox2.TabIndex = 5;
@@ -148,7 +148,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 36);
+            this.label3.Location = new System.Drawing.Point(3, 22);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(31, 13);
             this.label3.TabIndex = 4;
@@ -166,12 +166,12 @@
             this.panel2.Controls.Add(this.label7);
             this.panel2.Location = new System.Drawing.Point(298, 67);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(297, 114);
+            this.panel2.Size = new System.Drawing.Size(297, 128);
             this.panel2.TabIndex = 8;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(239, 70);
+            this.button6.Location = new System.Drawing.Point(130, 100);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(55, 23);
             this.button6.TabIndex = 9;
@@ -180,16 +180,16 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(185, 70);
+            this.button7.Location = new System.Drawing.Point(69, 100);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(55, 23);
             this.button7.TabIndex = 9;
-            this.button7.Text = "edit";
+            this.button7.Text = "Edit";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(131, 70);
+            this.button5.Location = new System.Drawing.Point(8, 100);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(55, 23);
             this.button5.TabIndex = 8;
@@ -245,21 +245,21 @@
             this.panel3.Controls.Add(this.panel1);
             this.panel3.Location = new System.Drawing.Point(12, 67);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(260, 246);
+            this.panel3.Size = new System.Drawing.Size(260, 263);
             this.panel3.TabIndex = 9;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(128, 36);
+            this.button2.Location = new System.Drawing.Point(92, 68);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 12;
-            this.button2.Text = "delete";
+            this.button2.Text = "Delete";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(180, 3);
+            this.button1.Location = new System.Drawing.Point(2, 68);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 11;
