9925847ca0184bca9d39abcb6c8118a7c75146c5
Manish <manish101096@gmail.com>
2016-05-09 13:50:40 -0700

    added updated Program Level class

--- ACD/ProgramLevel.cs
+++ ACD/ProgramLevel.cs
@@ -1,4 +1,5 @@
-﻿/// <summary>
+﻿using System;
+/// <summary>
 /// Author: Manish Mallavarapu 
 /// Date: 2016-05-09
 /// </summary>
@@ -32,22 +33,57 @@ namespace ACD
         public string name
         {
             get { return name_; }
-            set { name_ = value; }
+            set
+            {
+                if (!isEmptyInput(ref value)) {
+                    throw new ArgumentNullException("value can not be null or empty");
+                }
+                name_ = value;
+            }
         }
         public string desc
         {
             get { return desc_; }
-            set { desc_ = value; }
+            set
+            {
+                if (!isEmptyInput(ref value))
+                {
+                    throw new ArgumentNullException("value can not be null or empty");
+                }
+                desc_ = value;
+            }
         }
         public string courseNo
         {
             get { return courseNo_; }
-            set { courseNo_ = value; }
+            set
+            {
+                if (!isEmptyInput(ref value))
+                {
+                    throw new ArgumentNullException("value can not be null or empty");
+                }
+                courseNo_ = value;
+            }
         }
         public string core
         {
             get { return core_; }
-            set { core_ = value; }
+            set
+            {
+                if (!isEmptyInput(ref value))
+                {
+                    throw new ArgumentNullException("value can not be null or empty");
+                }
+                core_ = value;
+            }
+        }
+        private bool isEmptyInput(ref string value)
+        {
+            if (!string.IsNullOrEmpty(value) || !string.IsNullOrWhiteSpace(value))
+            {
+                return false; 
+            }
+            return true; 
         }
     }
 }
