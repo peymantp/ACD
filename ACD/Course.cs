f1719891c714142fceda28acf2c80361c5674e48
peymantp <peymantparsa@gmail.com>
2016-05-09 23:41:40 -0700

    fixed infinite loop

--- ACD/Course.cs
+++ ACD/Course.cs
@@ -8,7 +8,13 @@ namespace ACD
 {
     public class Course
     {
-
+        public List<Course> Prerequisites = new List<Course>();
+        private string _courseNumber;
+        private string _core;
+        private string _courseName;
+        private string _description;
+        private string _format;
+        private double _creditHours;
         public Course(string CourseNumber, string Core, string CourseName, string Description, double CreditHours, string Format, Course[] Prerequisities, bool HasLab)
         {
             this.CourseNumber = CourseNumber;
@@ -97,17 +103,17 @@ namespace ACD
         public Course(string CourseNumber, string Core, string CourseName, string Description, double CreditHours)
         {
             this.CourseNumber = CourseNumber;
-            this.CourseNumber = CourseNumber;
             this.Core = Core;
             this.CourseName = CourseName;
             this.Description = Description;
             this.CreditHours = CreditHours;
         }
-        public List<Course> Prerequisites = new List<Course>();
+        
         public bool HasLab { get; set; }
+        
         public string CourseNumber
         {
-            get { return CourseNumber; }
+            get { return _courseNumber; }
             set
             {
                 if (value.Length > 10)
@@ -115,16 +121,17 @@ namespace ACD
                     //System.Windows.Forms.MessageBox.Show("Course number to large");
                     throw new Exception("Course number to large");
                 }
-                if (!String.IsNullOrEmpty(value) || !String.IsNullOrWhiteSpace(value))
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
                 {
                     throw new ArgumentNullException("value can not be null or empty");
                 }
-                CourseNumber = value;
+                _courseNumber = value;
             }
         }
+        
         public string Core
         {
-            get { return Core; }
+            get { return _core; }
             set
             {
                 if (value.Length > 50)
@@ -132,16 +139,16 @@ namespace ACD
                     //System.Windows.Forms.MessageBox.Show("Course number to large");
                     throw new Exception("Core name to large");
                 }
-                if (!String.IsNullOrEmpty(value) || !String.IsNullOrWhiteSpace(value))
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
                 {
                     throw new ArgumentNullException("value can not be null or empty");
                 }
-                Core = value;
+                _core = value;
             }
         }
         public string CourseName
         {
-            get { return CourseName; }
+            get { return _courseName; }
             set
             {
                 if (value.Length > 50)
@@ -149,31 +156,31 @@ namespace ACD
                     //System.Windows.Forms.MessageBox.Show("Course number to large");
                     throw new Exception("Course name to large");
                 }
-                if (!String.IsNullOrEmpty(value) || !String.IsNullOrWhiteSpace(value))
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
                 {
                     throw new ArgumentNullException("value can not be null or empty");
                 }
-                CourseName = value;
+                _courseName = value;
             }
         }
         public string Description
         {
             get
             {
-                return Description;
+                return _description;
             }
             set
             {
-                if (!String.IsNullOrEmpty(value) || !String.IsNullOrWhiteSpace(value))
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
                 {
                     throw new ArgumentNullException("value can not be null or empty");
                 }
-                Description = value;
+                _description = value;
             }
         }
         public double CreditHours
         {
-            get { return CreditHours; }
+            get { return _creditHours; }
             set
             {
                 if (value < 1)
@@ -181,12 +188,12 @@ namespace ACD
                     //System.Windows.Forms.MessageBox.Show("Course number to large");
                     throw new Exception("Can't have 0 hours for a Course");
                 }
-                CreditHours = value;
+                _creditHours = value;
             }
         }
         public string Format
         {
-            get { return Format; }
+            get { return _format; }
             set
             {
                 if (value.Length > 50)
@@ -194,11 +201,11 @@ namespace ACD
                     //System.Windows.Forms.MessageBox.Show("Course number to large");
                     throw new Exception("Format name to large");
                 }
-                if (!String.IsNullOrEmpty(value) || !String.IsNullOrWhiteSpace(value))
+                if (String.IsNullOrEmpty(value) || String.IsNullOrWhiteSpace(value))
                 {
                     throw new ArgumentNullException("value can not be null or empty");
                 }
-                Format = value;
+                _format = value;
             }
         }
     }
