623f8bf1e8a1a6d004e682aa5aea8b5e7cd57bdb
peymantp <peymantparsa@gmail.com>
2016-05-10 14:18:26 -0700

    made course creation form material design

--- ACD/CourseForm.cs
+++ ACD/CourseForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,7 +11,7 @@ using System.Windows.Forms;
 
 namespace ACD
 {
-    public partial class CourseForm : Form
+    public partial class CourseForm : MaterialForm
     {
         public CourseForm()
         {
