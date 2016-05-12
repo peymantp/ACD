acd7eb82a3ec4700a4e8d365810370a43e9df13c
JustinHuchet <judahu@gmail.com>
2016-05-11 18:35:48 -0700

    Progress on y axis

--- Justin/CreateExcelWorksheet/CreateExcelWorksheet/Program.cs
+++ Justin/CreateExcelWorksheet/CreateExcelWorksheet/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 using Microsoft.Office.Interop.Excel;
 using System.Data.SqlClient;
 using System.Diagnostics;
@@ -8,7 +7,7 @@ public class CreateExcelWorksheet
 {
     static Worksheet ws;
     static String faculty;
-    static readonly int cellSize = 18;
+    static readonly int CELL_SIZE = 18;
 
     static void Main()
     {
@@ -20,6 +19,7 @@ public class CreateExcelWorksheet
             myConnection.Open();
 
             GenerateXAxis(myConnection);
+            GenerateYAxis(myConnection);
 
             myConnection.Close();
         }
@@ -97,21 +97,21 @@ public class CreateExcelWorksheet
         Range progRange = ws.get_Range("B1", GetExcelColumnName(performanceIndicatorsCount + 3) + "1");
         Range perfRange = ws.get_Range("B2", GetExcelColumnName(performanceIndicatorsCount + 3) + "2");
         
-        progRange.ColumnWidth = cellSize;
-        progRange.RowHeight = cellSize * 5;
-
-        perfRange.ColumnWidth = cellSize;
-        perfRange.RowHeight = cellSize * 5;
-
+        progRange.ColumnWidth = CELL_SIZE;
+        progRange.RowHeight = CELL_SIZE * 5;
+        
+        perfRange.ColumnWidth = CELL_SIZE;
+        perfRange.RowHeight = CELL_SIZE * 5;
+        
         progRange.WrapText = true;
         perfRange.WrapText = true;
 
         progRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
         progRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
-
+        
         perfRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
         perfRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
-
+        
         int i = 3;
         int j = performanceIndicatorsCount + 3;
         int k = j;
@@ -120,7 +120,6 @@ public class CreateExcelWorksheet
         {
             while (oReader.Read())
             {
-
                 progRange[i].Value2 = oReader["ProgramLevel"].ToString();
                 perfRange[i].Value2 = oReader["PerformanceIndicator"].ToString();
                 i++;
@@ -146,7 +145,6 @@ public class CreateExcelWorksheet
 
         for (i = 4; i <= performanceIndicatorsCount + 3; i++)
         {
-            Debug.WriteLine((string)(ws.Cells[1, i] as Range).Value);
             if (!String.IsNullOrEmpty((string)(ws.Cells[1, i] as Range).Value))
             {
                 if (!((string)(ws.Cells[1, i] as Range).Value).Contains(":"))
@@ -156,6 +154,138 @@ public class CreateExcelWorksheet
 
         Range nullDescriptions = ws.get_Range(GetExcelColumnName(i) + "1", GetExcelColumnName(performanceIndicatorsCount + 3) + "1");
         nullDescriptions.Font.Bold = true;
+    }
+
+    private static void GenerateYAxis(SqlConnection myConnection)
+    {
+        string oString;
+        SqlCommand oCmd;
+        
+        int numCourses = 0;
+        int numAllCourses = 0;
+
+        oString = "select sum(a.NumCourses) as AllCourses from (select grp.Name, COUNT(crs.Name) + AVG(grp.NumberOfElectives) as NumCourses from Course crs left join CourseGroup grp on SUBSTRING(crs.\"Key\", 1, CASE CHARINDEX('_', crs.\"Key\") WHEN 0 THEN LEN(crs.\"Key\") ELSE CHARINDEX('_', crs.\"Key\")-1 END) = grp.\"Key\" and SUBSTRING(crs.\"Key\", CASE CHARINDEX('_', crs.\"Key\") WHEN 0 THEN LEN(crs.\"Key\")+1 ELSE CHARINDEX('_', crs.\"Key\")+1 END, 1000) = grp.\"Name\" where grp.\"Key\" = '" + faculty + "' group by grp.Name) a";
+        oCmd = new SqlCommand(oString, myConnection);
+
+        using (SqlDataReader oReader = oCmd.ExecuteReader())
+        {
+            while (oReader.Read())
+            {
+                numAllCourses = Int32.Parse(oReader["AllCourses"].ToString());
+            }
+        }
+
+        Range groupRange = ws.get_Range("A3", "A" + (numAllCourses + 2).ToString());
+        Range courseNumRange = ws.get_Range("B3", "B" + (numAllCourses + 2).ToString());
+        Range courseNameRange = ws.get_Range("C3", "C" + (numAllCourses + 2).ToString());
+
+        groupRange.ColumnWidth = CELL_SIZE;
+        groupRange.RowHeight = CELL_SIZE * 5;
+
+        courseNumRange.ColumnWidth = CELL_SIZE;
+        courseNumRange.RowHeight = CELL_SIZE * 5;
+
+        courseNameRange.ColumnWidth = CELL_SIZE;
+        courseNameRange.RowHeight = CELL_SIZE * 5;
+
+        groupRange.WrapText = true;
+        courseNumRange.WrapText = true;
+        courseNameRange.WrapText = true;
+
+        groupRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+        groupRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+        courseNumRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+        courseNumRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+        courseNameRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+        courseNameRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+        groupRange.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+        groupRange.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+        groupRange.Interior.Color = XlRgbColor.rgbLightBlue;
+        groupRange.Font.Bold = true;
+
+        //oString = "select grp.Name, COUNT(crs.Name) + AVG(grp.NumberOfElectives) as NumCourses from Course crs left join CourseGroup grp on SUBSTRING(crs.\"Key\", 1, CASE CHARINDEX('_', crs.\"Key\") WHEN 0 THEN LEN(crs.\"Key\") ELSE CHARINDEX('_', crs.\"Key\")-1 END) = grp.\"Key\" and SUBSTRING(crs.\"Key\", CASE CHARINDEX('_', crs.\"Key\") WHEN 0 THEN LEN(crs.\"Key\")+1 ELSE CHARINDEX('_', crs.\"Key\")+1 END, 1000) = grp.\"Name\" where grp.\"Key\" = '" + faculty + "' group by grp.Name order by 2 desc,1";
+        oString = "select grp.Name as \"Group\", crs.Name as \"Course\", grp.NumberOfElectives,  ROW_NUMBER() over (partition by grp.\"Key\" order by grp.\"Name\", crs.Name) as num1 from Course crs left join CourseGroup grp on SUBSTRING(crs.\"Key\", 1, CASE CHARINDEX('_', crs.\"Key\") WHEN 0 THEN LEN(crs.\"Key\") ELSE CHARINDEX('_', crs.\"Key\")-1 END) = grp.\"Key\" and SUBSTRING(crs.\"Key\", CASE CHARINDEX('_', crs.\"Key\") WHEN 0 THEN LEN(crs.\"Key\")+1 ELSE CHARINDEX('_', crs.\"Key\")+1 END, 1000) = grp.\"Name\" where grp.\"Key\" = '" + faculty + "' order by 3";
+        oCmd = new SqlCommand(oString, myConnection);
+
+        int i = 1;
+        int j = numAllCourses + 2;
+        int k = j;
+
+        String lastGroup = null;
+        int lastNumElectives = 0;
+
+        using (SqlDataReader oReader = oCmd.ExecuteReader())
+        {
+            while (oReader.Read())
+            {
+                Debug.WriteLine("i: " + i);
+                Debug.WriteLine("numCourses: " + numCourses);
+                Debug.WriteLine("lastNumElectives: " + lastNumElectives);
+                Debug.WriteLine("lastGroup: " + lastGroup);
+                Debug.WriteLine("thisGroup: " + oReader["Group"].ToString());
+                Debug.WriteLine("");
+
+                if (i > 1 && !lastGroup.Equals(oReader["Group"].ToString()))
+                {
+                    if (lastNumElectives > 0)
+                    {
+                        for (int l = numCourses; l < lastNumElectives + numCourses; l++)
+                        {
+                            groupRange[i].Value2 = lastGroup;
+                            courseNumRange[i].Value2 = l;
+                            i++;
+                        }
+                    }
+
+                    groupRange[i].Value2 = oReader["Group"].ToString();
+                    courseNumRange[i].Value2 = "Course " + oReader["num1"].ToString();
+                    courseNameRange[i].Value2 = oReader["Course"].ToString();
+
+                    numCourses = 1;
+                }
+                else
+                {
+                    groupRange[i].Value2 = oReader["Group"].ToString();
+                    courseNumRange[i].Value2 = "Course " + oReader["num1"].ToString();
+                    courseNameRange[i].Value2 = oReader["Course"].ToString();
+                }
+
+                lastGroup = oReader["Group"].ToString();
+                lastNumElectives = Int32.Parse(oReader["NumberOfElectives"].ToString());
+                numCourses++;
+                i++;
+            }
+        }
+        if (lastNumElectives > 0)
+        {
+            for (int l = numCourses; l < lastNumElectives + numCourses; l++)
+            {
+                groupRange[i].Value2 = lastGroup;
+                courseNumRange[i].Value2 = l;
+                i++;
+            }
+        }
+
+        while (true)
+        {
+            if (j == 3)
+            {
+                ws.Range[ws.Cells[k, 1], ws.Cells[3, 1]].Merge();
+                k = j - 1;
+                break;
+            }
+            else if (!(((string)(ws.Cells[j - 1, 1] as Range).Value).Equals((string)(ws.Cells[j, 1] as Range).Value)))
+            {
+                ws.Range[ws.Cells[k, 1], ws.Cells[j, 1]].Merge();
+                k = j - 1;
+            }
+
+            j--;
+        }
     }
 
     private static string GetExcelColumnName(int columnNumber)
