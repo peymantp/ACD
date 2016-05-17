760df3b2e8037b2151d899eb8f37efe761346a27
peymantp <peymantparsa@gmail.com>
2016-05-17 13:22:37 -0700

    commented errorDialog

--- ACD/ErrorDialog.cs
+++ ACD/ErrorDialog.cs
@@ -11,8 +11,16 @@ using MaterialSkin.Controls;
 
 namespace ACD
 {
+    /// <summary>
+    /// This form is used to diplay errors in a way
+    /// that match the design of the rest of the program
+    /// </summary>
     public partial class ErrorDialog : MaterialForm
     {
+        /// <summary>
+        /// ctor for form
+        /// </summary>
+        /// <param name="msg">The error that will be displayed</param>
         public ErrorDialog(string msg)
         {
             InitializeComponent();
