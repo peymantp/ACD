f32ee6cd4468e2d4451f6b57860bbadab4d52c99
peymantp <peymantparsa@gmail.com>
2016-05-17 04:00:29 -0700

    created a form for showing error msgs in a form that matches the  rest of the project

---
+++ ACD/ErrorDialog.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ACD
+{
+    partial class ErrorDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.warningBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // warningBox
+            // 
+            this.warningBox.BackColor = System.Drawing.SystemColors.Control;
+            this.warningBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.warningBox.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.warningBox.Location = new System.Drawing.Point(1, 65);
+            this.warningBox.Name = "warningBox";
+            this.warningBox.ReadOnly = true;
+            this.warningBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.warningBox.Size = new System.Drawing.Size(417, 160);
+            this.warningBox.TabIndex = 18;
+            this.warningBox.Text = "";
+            // 
+            // ErrorDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 227);
+            this.Controls.Add(this.warningBox);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(417, 227);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(417, 227);
+            this.Name = "ErrorDialog";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "ErrorDialog";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox warningBox;
+    }
+}
