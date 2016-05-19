e2a9e589442a2e99a9d88c68887f3021b25749c0
manish mallavarapu <manish101096@gmail.com>
2016-05-19 12:32:52 -0700

    Refined Performance Indicator GUI

--- ACD/PerformanceIndicatorForm.Designer.cs
+++ ACD/PerformanceIndicatorForm.Designer.cs
@@ -56,12 +56,11 @@
             // ButtonSave
             // 
             this.ButtonSave.Depth = 0;
-            this.ButtonSave.Location = new System.Drawing.Point(118, 802);
-            this.ButtonSave.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonSave.Location = new System.Drawing.Point(411, 677);
             this.ButtonSave.MouseState = MaterialSkin.MouseState.HOVER;
             this.ButtonSave.Name = "ButtonSave";
             this.ButtonSave.Primary = true;
-            this.ButtonSave.Size = new System.Drawing.Size(173, 43);
+            this.ButtonSave.Size = new System.Drawing.Size(130, 35);
             this.ButtonSave.TabIndex = 11;
             this.ButtonSave.Text = "Save";
             this.ButtonSave.UseVisualStyleBackColor = true;
@@ -70,12 +69,11 @@
             // ButtonCancel
             // 
             this.ButtonCancel.Depth = 0;
-            this.ButtonCancel.Location = new System.Drawing.Point(327, 802);
-            this.ButtonCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.ButtonCancel.Location = new System.Drawing.Point(559, 677);
             this.ButtonCancel.MouseState = MaterialSkin.MouseState.HOVER;
             this.ButtonCancel.Name = "ButtonCancel";
             this.ButtonCancel.Primary = true;
-            this.ButtonCancel.Size = new System.Drawing.Size(173, 43);
+            this.ButtonCancel.Size = new System.Drawing.Size(130, 35);
             this.ButtonCancel.TabIndex = 10;
             this.ButtonCancel.Text = "Cancel";
             this.ButtonCancel.UseVisualStyleBackColor = true;
@@ -86,8 +84,7 @@
             this.TextFieldProgramLevel.Depth = 0;
             this.TextFieldProgramLevel.Enabled = false;
             this.TextFieldProgramLevel.Hint = "";
-            this.TextFieldProgramLevel.Location = new System.Drawing.Point(7, 176);
-            this.TextFieldProgramLevel.Margin = new System.Windows.Forms.Padding(4);
+            this.TextFieldProgramLevel.Location = new System.Drawing.Point(13, 167);
             this.TextFieldProgramLevel.MaxLength = 32767;
             this.TextFieldProgramLevel.MouseState = MaterialSkin.MouseState.HOVER;
             this.TextFieldProgramLevel.Name = "TextFieldProgramLevel";
@@ -95,7 +92,7 @@
             this.TextFieldProgramLevel.SelectedText = "";
             this.TextFieldProgramLevel.SelectionLength = 0;
             this.TextFieldProgramLevel.SelectionStart = 0;
-            this.TextFieldProgramLevel.Size = new System.Drawing.Size(491, 28);
+            this.TextFieldProgramLevel.Size = new System.Drawing.Size(368, 23);
             this.TextFieldProgramLevel.TabIndex = 9;
             this.TextFieldProgramLevel.TabStop = false;
             this.TextFieldProgramLevel.UseSystemPasswordChar = false;
@@ -105,8 +102,7 @@
             this.TextFieldProgram.Depth = 0;
             this.TextFieldProgram.Enabled = false;
             this.TextFieldProgram.Hint = "";
-            this.TextFieldProgram.Location = new System.Drawing.Point(7, 105);
-            this.TextFieldProgram.Margin = new System.Windows.Forms.Padding(4);
+            this.TextFieldProgram.Location = new System.Drawing.Point(13, 107);
             this.TextFieldProgram.MaxLength = 32767;
             this.TextFieldProgram.MouseState = MaterialSkin.MouseState.HOVER;
             this.TextFieldProgram.Name = "TextFieldProgram";
@@ -114,7 +110,7 @@
             this.TextFieldProgram.SelectedText = "";
             this.TextFieldProgram.SelectionLength = 0;
             this.TextFieldProgram.SelectionStart = 0;
-            this.TextFieldProgram.Size = new System.Drawing.Size(491, 28);
+            this.TextFieldProgram.Size = new System.Drawing.Size(368, 23);
             this.TextFieldProgram.TabIndex = 8;
             this.TextFieldProgram.TabStop = false;
             this.TextFieldProgram.UseSystemPasswordChar = false;
@@ -125,11 +121,10 @@
             this.labelProgram.Depth = 0;
             this.labelProgram.Font = new System.Drawing.Font("Roboto", 11F);
             this.labelProgram.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.labelProgram.Location = new System.Drawing.Point(8, 77);
-            this.labelProgram.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelProgram.Location = new System.Drawing.Point(6, 85);
             this.labelProgram.MouseState = MaterialSkin.MouseState.HOVER;
             this.labelProgram.Name = "labelProgram";
-            this.labelProgram.Size = new System.Drawing.Size(83, 24);
+            this.labelProgram.Size = new System.Drawing.Size(66, 19);
             this.labelProgram.TabIndex = 13;
             this.labelProgram.Text = "Program";
             // 
@@ -139,11 +134,10 @@
             this.materialLabel2.Depth = 0;
             this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel2.Location = new System.Drawing.Point(8, 148);
-            this.materialLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel2.Location = new System.Drawing.Point(6, 145);
             this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel2.Name = "materialLabel2";
-            this.materialLabel2.Size = new System.Drawing.Size(224, 24);
+            this.materialLabel2.Size = new System.Drawing.Size(178, 19);
             this.materialLabel2.TabIndex = 15;
             this.materialLabel2.Text = "Program Level Outcomes";
             // 
@@ -153,11 +147,10 @@
             this.materialLabel3.Depth = 0;
             this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel3.Location = new System.Drawing.Point(8, 294);
-            this.materialLabel3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel3.Location = new System.Drawing.Point(6, 264);
             this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel3.Name = "materialLabel3";
-            this.materialLabel3.Size = new System.Drawing.Size(135, 24);
+            this.materialLabel3.Size = new System.Drawing.Size(109, 19);
             this.materialLabel3.TabIndex = 16;
             this.materialLabel3.Text = "Level 1 Criteria";
             // 
@@ -167,11 +160,10 @@
             this.materialLabel4.Depth = 0;
             this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel4.Location = new System.Drawing.Point(8, 421);
-            this.materialLabel4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel4.Location = new System.Drawing.Point(6, 376);
             this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel4.Name = "materialLabel4";
-            this.materialLabel4.Size = new System.Drawing.Size(135, 24);
+            this.materialLabel4.Size = new System.Drawing.Size(109, 19);
             this.materialLabel4.TabIndex = 18;
             this.materialLabel4.Text = "Level 2 Criteria";
             // 
@@ -181,11 +173,10 @@
             this.materialLabel5.Depth = 0;
             this.materialLabel5.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel5.Location = new System.Drawing.Point(8, 549);
-            this.materialLabel5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel5.Location = new System.Drawing.Point(6, 487);
             this.materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel5.Name = "materialLabel5";
-            this.materialLabel5.Size = new System.Drawing.Size(135, 24);
+            this.materialLabel5.Size = new System.Drawing.Size(109, 19);
             this.materialLabel5.TabIndex = 19;
             this.materialLabel5.Text = "Level 3 Criteria";
             // 
@@ -195,11 +186,10 @@
             this.materialLabel6.Depth = 0;
             this.materialLabel6.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel6.Location = new System.Drawing.Point(8, 667);
-            this.materialLabel6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel6.Location = new System.Drawing.Point(6, 610);
             this.materialLabel6.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel6.Name = "materialLabel6";
-            this.materialLabel6.Size = new System.Drawing.Size(135, 24);
+            this.materialLabel6.Size = new System.Drawing.Size(109, 19);
             this.materialLabel6.TabIndex = 20;
             this.materialLabel6.Text = "Level 4 Criteria";
             // 
@@ -209,11 +199,10 @@
             this.materialLabel1.Depth = 0;
             this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel1.Location = new System.Drawing.Point(8, 221);
-            this.materialLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel1.Location = new System.Drawing.Point(6, 207);
             this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel1.Name = "materialLabel1";
-            this.materialLabel1.Size = new System.Drawing.Size(136, 24);
+            this.materialLabel1.Size = new System.Drawing.Size(109, 19);
             this.materialLabel1.TabIndex = 24;
             this.materialLabel1.Text = "Indicator name";
             // 
@@ -221,8 +210,7 @@
             // 
             this.indicatorNameField.Depth = 0;
             this.indicatorNameField.Hint = "";
-            this.indicatorNameField.Location = new System.Drawing.Point(7, 249);
-            this.indicatorNameField.Margin = new System.Windows.Forms.Padding(4);
+            this.indicatorNameField.Location = new System.Drawing.Point(13, 229);
             this.indicatorNameField.MaxLength = 32767;
             this.indicatorNameField.MouseState = MaterialSkin.MouseState.HOVER;
             this.indicatorNameField.Name = "indicatorNameField";
@@ -230,7 +218,7 @@
             this.indicatorNameField.SelectedText = "";
             this.indicatorNameField.SelectionLength = 0;
             this.indicatorNameField.SelectionStart = 0;
-            this.indicatorNameField.Size = new System.Drawing.Size(491, 28);
+            this.indicatorNameField.Size = new System.Drawing.Size(368, 23);
             this.indicatorNameField.TabIndex = 25;
             this.indicatorNameField.TabStop = false;
             this.indicatorNameField.UseSystemPasswordChar = false;
@@ -242,40 +230,36 @@
             // criteria1TextField
             // 
             this.criteria1TextField.BackColor = System.Drawing.SystemColors.Control;
-            this.criteria1TextField.Location = new System.Drawing.Point(7, 322);
-            this.criteria1TextField.Margin = new System.Windows.Forms.Padding(4);
+            this.criteria1TextField.Location = new System.Drawing.Point(12, 286);
             this.criteria1TextField.Name = "criteria1TextField";
-            this.criteria1TextField.Size = new System.Drawing.Size(491, 86);
+            this.criteria1TextField.Size = new System.Drawing.Size(369, 71);
             this.criteria1TextField.TabIndex = 26;
             this.criteria1TextField.Text = "";
             // 
             // criteria2TextField
             // 
             this.criteria2TextField.BackColor = System.Drawing.SystemColors.Control;
-            this.criteria2TextField.Location = new System.Drawing.Point(7, 449);
-            this.criteria2TextField.Margin = new System.Windows.Forms.Padding(4);
+            this.criteria2TextField.Location = new System.Drawing.Point(12, 398);
             this.criteria2TextField.Name = "criteria2TextField";
-            this.criteria2TextField.Size = new System.Drawing.Size(491, 86);
+            this.criteria2TextField.Size = new System.Drawing.Size(369, 71);
             this.criteria2TextField.TabIndex = 27;
             this.criteria2TextField.Text = "";
             // 
             // criteria3TextField
             // 
             this.criteria3TextField.BackColor = System.Drawing.SystemColors.Control;
-            this.criteria3TextField.Location = new System.Drawing.Point(7, 577);
-            this.criteria3TextField.Margin = new System.Windows.Forms.Padding(4);
+            this.criteria3TextField.Location = new System.Drawing.Point(13, 520);
             this.criteria3TextField.Name = "criteria3TextField";
-            this.criteria3TextField.Size = new System.Drawing.Size(491, 86);
+            this.criteria3TextField.Size = new System.Drawing.Size(369, 71);
             this.criteria3TextField.TabIndex = 28;
             this.criteria3TextField.Text = "";
             // 
             // criteria4TextField
             // 
             this.criteria4TextField.BackColor = System.Drawing.SystemColors.Control;
-            this.criteria4TextField.Location = new System.Drawing.Point(7, 695);
-            this.criteria4TextField.Margin = new System.Windows.Forms.Padding(4);
+            this.criteria4TextField.Location = new System.Drawing.Point(13, 641);
             this.criteria4TextField.Name = "criteria4TextField";
-            this.criteria4TextField.Size = new System.Drawing.Size(491, 86);
+            this.criteria4TextField.Size = new System.Drawing.Size(369, 71);
             this.criteria4TextField.TabIndex = 29;
             this.criteria4TextField.Text = "";
             // 
@@ -285,11 +269,10 @@
             this.courseLabel.Depth = 0;
             this.courseLabel.Font = new System.Drawing.Font("Roboto", 11F);
             this.courseLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.courseLabel.Location = new System.Drawing.Point(527, 77);
-            this.courseLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.courseLabel.Location = new System.Drawing.Point(451, 85);
             this.courseLabel.MouseState = MaterialSkin.MouseState.HOVER;
             this.courseLabel.Name = "courseLabel";
-            this.courseLabel.Size = new System.Drawing.Size(79, 24);
+            this.courseLabel.Size = new System.Drawing.Size(65, 19);
             this.courseLabel.TabIndex = 30;
             this.courseLabel.Text = "Courses";
             // 
@@ -299,11 +282,10 @@
             this.materialLabel8.Depth = 0;
             this.materialLabel8.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel8.Location = new System.Drawing.Point(847, 77);
-            this.materialLabel8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel8.Location = new System.Drawing.Point(673, 85);
             this.materialLabel8.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel8.Name = "materialLabel8";
-            this.materialLabel8.Size = new System.Drawing.Size(15, 24);
+            this.materialLabel8.Size = new System.Drawing.Size(13, 19);
             this.materialLabel8.TabIndex = 31;
             this.materialLabel8.Text = "I";
             // 
@@ -313,11 +295,10 @@
             this.materialLabel9.Depth = 0;
             this.materialLabel9.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel9.Location = new System.Drawing.Point(870, 77);
-            this.materialLabel9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel9.Location = new System.Drawing.Point(692, 85);
             this.materialLabel9.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel9.Name = "materialLabel9";
-            this.materialLabel9.Size = new System.Drawing.Size(22, 24);
+            this.materialLabel9.Size = new System.Drawing.Size(18, 19);
             this.materialLabel9.TabIndex = 32;
             this.materialLabel9.Text = "R";
             // 
@@ -327,19 +308,18 @@
             this.materialLabel10.Depth = 0;
             this.materialLabel10.Font = new System.Drawing.Font("Roboto", 11F);
             this.materialLabel10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.materialLabel10.Location = new System.Drawing.Point(900, 77);
-            this.materialLabel10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.materialLabel10.Location = new System.Drawing.Point(716, 85);
             this.materialLabel10.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialLabel10.Name = "materialLabel10";
-            this.materialLabel10.Size = new System.Drawing.Size(22, 24);
+            this.materialLabel10.Size = new System.Drawing.Size(19, 19);
             this.materialLabel10.TabIndex = 33;
             this.materialLabel10.Text = "D";
             // 
             // PerformanceIndicatorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(935, 858);
+            this.ClientSize = new System.Drawing.Size(761, 727);
             this.Controls.Add(this.materialLabel10);
             this.Controls.Add(this.materialLabel9);
             this.Controls.Add(this.materialLabel8);
@@ -360,10 +340,9 @@
             this.Controls.Add(this.ButtonCancel);
             this.Controls.Add(this.TextFieldProgramLevel);
             this.Controls.Add(this.TextFieldProgram);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(532, 338);
+            this.MinimumSize = new System.Drawing.Size(399, 275);
             this.Name = "PerformanceIndicatorForm";
             this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
